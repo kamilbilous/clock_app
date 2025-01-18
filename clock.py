ecb09a16aa5288be6d120eaf8a947c81605a371f
kamilbilous <bruhleq@gmail.com>
2025-01-18 18:36:32 +0100

    feat: added TODO to clock

--- clock.py
+++ clock.py
@@ -2,6 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import QTimer, QTime, Qt
 
+#TODO : Diff timezones
+#TODO : Analog clock?
+#TODO : Changable colors?
 
 
 class Clock(QWidget):
